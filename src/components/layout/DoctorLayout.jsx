f74429a2dbf52265189a79640b16ce5e7a715b44
alinasr783 <alinasreldin783@gmail.com>
2025-12-17 03:48:14 +0200

    slaider

--- src/components/layout/DoctorLayout.jsx
+++ src/components/layout/DoctorLayout.jsx
@@ -180,6 +180,7 @@ export default function DoctorLayout() {
             isVisible={hasPatientsAccess}
             onClick={handleNavItemClick}
           />
+          
           <NavItem
             to="/clinic"
             icon={Building}
